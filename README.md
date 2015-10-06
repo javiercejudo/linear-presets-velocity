a4ea064e88838e363f01373e97f1bf63212b7e85
Javier Cejudo <javier@javiercejudo.com>
2015-10-07 01:11:17 +1100

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# linear-presets-velocity
+
+[![Build Status](https://travis-ci.org/javiercejudo/linear-presets-velocity.svg)](https://travis-ci.org/javiercejudo/linear-presets-velocity)
+[![Coverage Status](https://coveralls.io/repos/javiercejudo/linear-presets-velocity/badge.svg?branch=master)](https://coveralls.io/r/javiercejudo/linear-presets-velocity?branch=master)
+[![Code Climate](https://codeclimate.com/github/javiercejudo/linear-presets-velocity/badges/gpa.svg)](https://codeclimate.com/github/javiercejudo/linear-presets-velocity)
+
+Linear presets for velocity units
+
+## Install
+
+    npm i linear-presets-velocity
+
+## Presets
+
+- [Metres per second](https://en.wikipedia.org/wiki/Metre_per_second)
+- [Miles per hour](https://en.wikipedia.org/wiki/Miles_per_hour)
+- [Feet per second](https://en.wikipedia.org/wiki/Foot_per_second)
+- [Kilometres per hour](https://en.wikipedia.org/wiki/Kilometres_per_hour)
+- [Knot](https://en.wikipedia.org/wiki/Knot_(unit))
+
+## Related projects
+
+- [linear-presets](https://github.com/javiercejudo/linear-presets): linear presets for common units.
+- [linear-converter](https://github.com/javiercejudo/linear-converter): flexible linear converter.
---
+++ test/presets.js
@@ -0,0 +1,31 @@
+/*jshint node:true, mocha:true */
+
+'use strict';
+
+require('should');
+
+var Decimal = require('linear-arbitrary-precision')(require('floating-adapter'));
+var rescale = require('rescale')(Decimal).rescale;
+var velocity = require('linear-preset-factory')(require('../src/linear-presets-velocity'));
+
+function convert(x, preset) {
+  return Number(rescale(preset[0], preset[1], x));
+}
+
+function invert(preset) {
+  return preset.slice(0).reverse();
+}
+
+describe('velocity presets', function() {
+  it('should convert correctly', function() {
+    (100/9.58).should.be.exactly(convert(23.35006567906474, invert(velocity.metresSecond_milesHour)), 'metresSecond_milesHour')
+      .and.exactly(convert(34.24676299596162, invert(velocity.metresSecond_feetSecond)), 'metresSecond_feetSecond')
+      .and.exactly(convert(37.578288100208766, invert(velocity.metresSecond_kilometresHour)), 'metresSecond_kilometresHour')
+      .and.exactly(convert(20.290652321926984, invert(velocity.metresSecond_knot)), 'metresSecond_knot');
+
+    (0).should.be.exactly(convert(0, velocity.metresSecond_milesHour), 'metresSecond_milesHour')
+      .and.exactly(convert(0, velocity.metresSecond_feetSecond), 'metresSecond_feetSecond')
+      .and.exactly(convert(0, velocity.metresSecond_kilometresHour), 'metresSecond_kilometresHour')
+      .and.exactly(convert(0, velocity.metresSecond_knot), 'metresSecond_knot');
+  });
+});
